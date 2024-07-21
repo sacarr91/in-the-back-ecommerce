4fe72f131dda4d3a2f285292b4a814eb2a25f8f7
Stephanie Carr <sacarr91@gmail.com>
2024-07-21 19:57:19 -0400

    corrected models/index.js many-to-many relationships

--- models/index.js
+++ models/index.js
@@ -17,15 +17,21 @@ Product.belongsTo(Category, {
 
 // Tag belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'product_id',
-});
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+    as: 'products_in_tag'
+}); 
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
-});
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tags_on-product'
+}); 
 
 module.exports = {
   Product,
