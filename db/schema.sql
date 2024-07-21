a037bd5bd7c10906c3edb663fddc806abbee5b24
Stephanie Carr <sacarr91@gmail.com>
2024-07-21 19:29:21 -0400

    added dotenv, updated connection.js, server.js, & Category and Product models

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,26 @@
+-- ///////// Stanley code ////////////
+
+/** Must add to disconnect session to this database execempt this one..
+Note: this script is ONLY for psql
+**/
+
+SELECT 'Database rebuild started...';
+DROP DATABASE IF EXISTS temp;
+CREATE DATABASE temp;
+
+\c temp;
+
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'university'
+AND pid <> pg_backend_pid();
+
+-- /////// End Stanley code //////////
+
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+\c ecommerce_db;
